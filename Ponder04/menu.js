87563060446348804789d75f553f9fede259e32d
ddurt <dur21020@byui.edu>
2026-05-12 10:02:07 -0600

    ponder'0'4

---
+++ Ponder04/menu.js
@@ -0,0 +1,15 @@
+let menuBtn = document.querySelector('.menu-btn');
+console.log(menuBtn);
+
+menuBtn.addEventListener("click", handleMenuBtnClick);
+
+// write a function
+function handleMenuBtnClick(event) {
+    //grab the nav tag
+    let nav = document.querySelector('nav');
+    // toggle .hide class on nav tag
+    nav.classList.toggle("hide");
+    // toggle animation
+    menuBtn.classList.toggle("change");
+}
+
